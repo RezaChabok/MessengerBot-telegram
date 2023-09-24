e601b6d85a5f66c93291fb40e9e960d20877b192
Reza Chabok <r.chabok@proton.me>
2023-09-24 17:19:07 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ proxy_url= "http://proxy.server:3128"
 telepot.api._pools = {
     'default': urllib3.ProxyManager(proxy_url= proxy_url, num_pools=3,maxsize=10, retries = False, timeout= 30),
 }
-
 telepot.api._onetime_pool_spec =(urllib3.ProxyManager, dict(proxy_url= proxy_url, num_pools=1, maxsize=1,retries=False , timeout=30))
 secret="bot"
 bot = telepot.Bot('Token')
@@ -22,7 +21,7 @@ def handle(msg):
             try:
                 bot.sendMessage(msg['reply_to_message']['forward_from']['id'],msg["text"])
                 bot.sendMessage(chat_id,'sent')
-            except KeyError :pass#For example, the admin replyes to a user message and sends hi, and the robot sends hi to the same user whose message was replyed.
+            except KeyError : pass #For example, the admin replyes to a user message and sends hi, and the robot sends hi to the same user whose message was replyed.
         bot.sendMessage('admin_chat_id',msg)
         mention="tg://openmessage?user_id={}".format(chat_id)
         bot.sendMessage('admin_chat_id',mention)
@@ -32,9 +31,8 @@ def handle(msg):
         bot.forwardMessage('admin_chat_id', msg['chat']['id'], msg['message_id'])
         bot.sendMessage(chat_id,"sent.")
 
-
-
 app = Flask(__name__)
+
 
 @app.route('/{}'.format(secret),methods = ["POST"])
 def telegram_webhook():
